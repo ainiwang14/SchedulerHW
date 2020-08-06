b885ebecdeb60cc4b18e9fa6c7f241dad81d16f1
AileenWang <ainiwang14@gmail.com>
2020-08-05 17:39:38 -0700

    -m

--- stuff.js
+++ stuff.js
@@ -1,10 +1,11 @@
-const timeBlocksCount = 12
+const timeBlocksCount = 13
 
 function makeTimeBlocks() {
 
     for (let i = 0; i < timeBlocksCount; i++) {
+        const hour = moment().startOf('day').add(i + 8, 'H').format('H');
         const timeBlock = $("<div>");
-        timeBlock.text("hello world")
+        timeBlock.text(hour);
         $(".container").append(timeBlock);
     }
 
