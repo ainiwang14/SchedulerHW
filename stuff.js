fba46836e8ae9f27cea2628a37d9cad36528cd90
AileenWang <ainiwang14@gmail.com>
2020-08-05 20:33:25 -0700

    doing some formatting

--- stuff.js
+++ stuff.js
@@ -8,34 +8,41 @@ function makeTimeBlocks() {
         timeBlockContainer.attr("id", i);
 
         timeBlockContainer
-            .append(makeTimeBlockHour())
+            .append(makeTimeBlockHour(i))
             .append(makeTimeBlockActivity())
-            .append(makeTimeBlockSave());
+            .append(makeTimeBlockSave(i));
         $(".container").append(timeBlockContainer);
-
     }
 
 }
 
-function makeTimeBlockHour() {
+function makeTimeBlockHour(counter) {
     const timeBlockHour = $("<div>");
-    const hour = moment().startOf('day').add(i + 8, 'h').format('h A');
+    const hour = moment().startOf('day').add(counter + 8, 'h').format('h A');
     timeBlockHour.text(hour);
-    timeBlockHour.attr("class", "inline");
+    timeBlockHour.attr("class", "inline hour");
     return timeBlockHour
 }
 
 function makeTimeBlockActivity() {
-    const timeBlockActivity = $("<div>");
+    const timeBlockActivity = $("<input>");
     timeBlockActivity.text("activity");
-    timeBlockActivity.attr("class", "inline");
+    timeBlockActivity.attr("class", "inline activity");
     return timeBlockActivity;
 }
 
-function makeTimeBlockSave() {
+function makeTimeBlockSave(counter) {
     const timeBlockSave = $("<button>");
     timeBlockSave.text("save");
-    timeBlockSave.attr("class", "inline");
+    timeBlockSave.attr("class", "inline save");
+
+    function onClick(counter) {
+        const savedActivityText = $("input.activity").val();
+        console.log(savedActivityText);
+        localStorage.setItem(counter, savedActivityText);
+    }
+
+    timeBlockSave.click(onClick);
     return timeBlockSave;
 }
 
