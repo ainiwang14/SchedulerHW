3d9acf5e261a2e8723c125e47f908e31ad7fea09
AileenWang <ainiwang14@gmail.com>
2020-08-05 18:09:58 -0700

    making functions

--- stuff.js
+++ stuff.js
@@ -3,20 +3,40 @@ const timeBlocksCount = 13
 function makeTimeBlocks() {
 
     for (let i = 0; i < timeBlocksCount; i++) {
-        const hour = moment().startOf('day').add(i + 8, 'h').format('h A');
         const timeBlockContainer = $("<div>");
-        const timeBlockHour = $("<div>");
-        const timeBlockActivity = $("<div>");
-        const timeBlockSave = $("<div>");
 
-        timeBlockHour.text(hour);
-        timeBlockActivity.text("activity");
-        timeBlockSave.text("save");
-        timeBlockContainer.append(timeBlockHour).append(timeBlockActivity).append(timeBlockSave);
+        timeBlockContainer.attr("id", i);
+
+        timeBlockContainer
+            .append(makeTimeBlockHour())
+            .append(makeTimeBlockActivity())
+            .append(makeTimeBlockSave());
         $(".container").append(timeBlockContainer);
 
     }
 
+}
+
+function makeTimeBlockHour() {
+    const timeBlockHour = $("<div>");
+    const hour = moment().startOf('day').add(i + 8, 'h').format('h A');
+    timeBlockHour.text(hour);
+    timeBlockHour.attr("class", "inline");
+    return timeBlockHour
+}
+
+function makeTimeBlockActivity() {
+    const timeBlockActivity = $("<div>");
+    timeBlockActivity.text("activity");
+    timeBlockActivity.attr("class", "inline");
+    return timeBlockActivity;
+}
+
+function makeTimeBlockSave() {
+    const timeBlockSave = $("<button>");
+    timeBlockSave.text("save");
+    timeBlockSave.attr("class", "inline");
+    return timeBlockSave;
 }
 
 makeTimeBlocks()
