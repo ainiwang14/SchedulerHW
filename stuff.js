d98e5076db91a0594883b2b42635314d31b5d70b
AileenWang <ainiwang14@gmail.com>
2020-08-05 17:49:16 -0700

    working on time loop

--- stuff.js
+++ stuff.js
@@ -3,10 +3,18 @@ const timeBlocksCount = 13
 function makeTimeBlocks() {
 
     for (let i = 0; i < timeBlocksCount; i++) {
-        const hour = moment().startOf('day').add(i + 8, 'H').format('H');
-        const timeBlock = $("<div>");
-        timeBlock.text(hour);
-        $(".container").append(timeBlock);
+        const hour = moment().startOf('day').add(i + 8, 'h').format('h A');
+        const timeBlockContainer = $("<div>");
+        const timeBlockHour = $("<div>");
+        const timeBlockActivity = $("<div>");
+        const timeBlockSave = $("<div>");
+
+        timeBlockHour.text(hour);
+        timeBlockActivity.text("activity");
+        timeBlockSave.text("save");
+        timeBlockContainer.append(timeBlockHour).append(timeBlockActivity).append(timeBlockSave);
+        $(".container").append(timeBlockContainer);
+
     }
 
 }
